8d8caf42215b8935bb83468c838b62668648effc
BarrelRider65 <862785121@qq.com>
2018-08-17 13:49:10 +0800

    starty and endy completed

--- app/src/main/java/com/me/harris/tipdemo/roundcircle/PanCakeView.kt
+++ app/src/main/java/com/me/harris/tipdemo/roundcircle/PanCakeView.kt
@@ -20,25 +20,19 @@ class PanCakeView @JvmOverloads constructor(
     var centerY = 0f
 
 
+
+
     // 可设置的参数
     var innerRadius = 0f //内圆半径
     var outterRadius = 0f //外圆半径
 
+    val offset = 30f//伸出的距离
+
     var startAngle = 0f //顺时针画圆弧开始的角度
-    var sweepAngle = 60f*Math.PI/180f //默认第一个圆弧是60度
+    var sweepAngle = 60f //默认第一个圆弧是60度
 
-    var firstArchColor = Color.parseColor("#F05A4A")//从startAngle开始，第一个圆弧的颜色
-    var secondArchColor = Color.parseColor("#29AB91")//第二个圆弧的颜色
-
-
-
-
-
-    fun initPaint(){
-        paint.style=Paint.Style.FILL
-        paint.color = firstArchColor
-        textPaint.strokeWidth=2f
-    }
+    var firstArchColor = Color.parseColor("#29AB91")//从startAngle开始，第一个圆弧的颜色
+    var secondArchColor = Color.parseColor("#F05A4A")//第二个圆弧的颜色
 
 
 
@@ -51,14 +45,20 @@ class PanCakeView @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas?) {
-        initPaint()
+
+        paint.style=Paint.Style.FILL
+        paint.color = firstArchColor
+        textPaint.strokeWidth=2f
         centerX = width /2f
         centerY = height /2f
         if (centerX<centerY){
-            outterRadius = centerX/2f
+            outterRadius = centerX
         }else {
-            outterRadius = centerY/2f
+            outterRadius = centerY
         }
+
+        outterRadius -= offset
+
 
         if (innerRadius<=0){
             innerRadius = outterRadius/2f
@@ -75,21 +75,21 @@ class PanCakeView @JvmOverloads constructor(
         //画绿色的圆环
 
         paint.color = secondArchColor
-        val secondRadius = outterRadius+20
+        val secondRadius = outterRadius+offset
         val startX = centerX-secondRadius*Math.sin((sweepAngle/2)*Math.PI/180)
         val startY = centerY-secondRadius*Math.cos((sweepAngle/2)*Math.PI/180)
 
 
         val endX = centerX+secondRadius*Math.sin((sweepAngle/2)*Math.PI/180)
-        val endY = centerY+secondRadius*Math.cos((sweepAngle/2)*Math.PI/180)
+        val endY = startY
 
         path.moveTo(centerX,centerY)
-        path.lineTo(startX.toFloat(), startY.toFloat())
-        path.lineTo(startX.toFloat(), startY.toFloat())
+        path.lineTo(startX.toFloat(),startY.toFloat())
+        path.lineTo(endX.toFloat(), endY.toFloat())
         path.close()
         val recf = RectF(centerX-secondRadius,centerY-secondRadius,
                 centerX+secondRadius,centerY+secondRadius)
-        path.addArc(recf,-120f,90f)
+        path.addArc(recf,-90f,sweepAngle.toFloat())
         canvas?.drawPath(path,paint)
         path.reset()
 
